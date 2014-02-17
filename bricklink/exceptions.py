5cc87bb1573a1bb70d178d0c293308d1e9f2b860
Robert de Vries <r.devries@robbytu.net>
2014-02-17 22:07:39 +0100

    Added Bricklink API exceptions

---
+++ bricklink/exceptions.py
@@ -0,0 +1,41 @@
+'''
+    bricklink.exceptions
+    --------------------
+
+    A module providing Bricklink exceptions
+'''
+
+class BricklinkInvalidResponseException(Exception): pass
+
+
+class BricklinkInvalidURIException(Exception): pass
+
+
+class BricklinkInvalidRequestBodyException(Exception): pass
+
+
+class BricklinkParameterMissingOrInvalidException(Exception): pass
+
+
+class BricklinkBadOauthRequestException(Exception): pass
+
+
+class BricklinkPermissionDeniedException(Exception): pass
+
+
+class BricklinkResourceNotFoundException(Exception): pass
+
+
+class BricklinkMethodNotAllowedException(Exception): pass
+
+
+class BricklinkUnsupportedMediaTypeException(Exception): pass
+
+
+class BricklinkResourceUpdateNotAllowedException(Exception): pass
+
+
+class BricklinkInternalServerErrorException(Exception): pass
+
+
+class BricklinkUnspecifiedException(Exception): pass
